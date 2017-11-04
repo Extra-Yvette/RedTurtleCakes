10cbc539ac25199b82ab6bddefd175ac86a435e8
Ray <ray00000sina@gmail.com>
2017-11-04 23:59:06 +0800

    fixed : 調整遊戲啟動後，遊戲視窗會在電腦螢幕正中間。

--- src/yvette/game/Main.java
+++ src/yvette/game/Main.java
@@ -1,12 +1,21 @@
 package yvette.game;
 
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 import yvette.game.control.RedTurtleCakes;
 
 public class Main {
 	public static void main(String[] args) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		System.out.println("Screen width = " + d.width);
+		System.out.println("Screen height = " + d.height);
+
+
 		RedTurtleCakes cakes = RedTurtleCakes.getInstance();
 
 		//載入設定檔
@@ -16,7 +25,10 @@ public class Main {
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(cakes.getGameCanvas());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100, 100, config.getScreenWidth(), config.getScreenHeight());
+		frame.setBounds((d.width / 2) - (config.getScreenWidth() / 2),
+				(d.height / 2) - (config.getScreenWidth() / 2),
+				config.getScreenWidth(),
+				config.getScreenHeight());
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
